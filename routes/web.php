7c3868b105563a5a2e39da9b9ba06b0840976ce9
Giovanni Aranda <giovanni.arandaa@gmail.com>
2025-03-07 09:52:29 -0500

    Route Model Binding with Parameters
    Summary of this lesson:
    - Create PostController to handle individual post display
    - Implement route with dynamic parameter using Route Model Binding
    - Create post View template with dynamic content rendering
    - Add navigation links to individual post pages

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
 Route::view('article', 'article')->name('article');
+Route::get('post/{post}', [PostController::class, 'show'])->name('post.show');
 
 
