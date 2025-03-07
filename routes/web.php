5f1b4a85581507c54b6a0e981592183aae2ab2ee
Giovanni Aranda <giovanni.arandaa@gmail.com>
2025-03-07 09:44:57 -0500

    MVC, DB Queries, and Eloquent Models

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'home')->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
 Route::view('article', 'article')->name('article');
